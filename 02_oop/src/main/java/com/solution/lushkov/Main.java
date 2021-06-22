e26a2b2d025ef7686053537902746cc662f68f7c
Victor Lushkov <viktor20010627@gmail.com>
2021-06-22 22:22:10 +0300

    created module oop

---
+++ 02_oop/src/main/java/com/solution/lushkov/Main.java
@@ -0,0 +1,13 @@
+package com.solution.lushkov;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.LogManager;
+
+public class Main {
+    private static final Logger LOG = LogManager
+            .getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        LOG.info("HiHi");
+    }
+}
